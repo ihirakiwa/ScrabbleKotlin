2317cb22bc319d0958a9121c5bfccf9060041f88
Matthieu <matthieu.espadamora@gmail.com>
2024-01-25 20:54:14 +0100

    coucou

--- Scrabble/app/build.gradle.kts
+++ Scrabble/app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
