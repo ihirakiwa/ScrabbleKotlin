d1ef5aa98efa2203d29699498421c5d73573bc97
ihirakiwa <killiann76170@gmail.com>
2024-02-02 21:11:20 +0100

    j'aime les pates

--- Scrabble/app/src/main/java/com/example/scrabble/BoardActivity.kt
+++ Scrabble/app/src/main/java/com/example/scrabble/BoardActivity.kt
@@ -10,9 +10,9 @@ import android.widget.GridView
 import android.widget.ListView
 
 class BoardActivity : AppCompatActivity() {
-    private val player1 : Player = intent.getSerializableExtra("player1") as Player
-    private val player2 : Player = intent.getSerializableExtra("player2") as Player
-    private lateinit var rack: Rack
+    private lateinit var player1: Player
+    private lateinit var player2: Player
+    private lateinit var rack1: Rack
 
 
 
@@ -20,17 +20,21 @@ class BoardActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board)
+        // Récupérer les joueurs de l'intent
+        player1 = intent.getSerializableExtra("player1") as Player
+        player2 = intent.getSerializableExtra("player2") as Player
+
         val rackView = findViewById<RackView>(R.id.rackView)
         val btnRefresh = findViewById<Button>(R.id.btnRefresh)
-        val player = Player("Joueur 1")
+
         val letterbag = LetterBag()
-        rack = Rack(player)
-        rack.drawMultipleLetters(LetterBag(), 7)
-        rackView.updateView(rack.getRack())
+        rack1 = Rack(player1)
+        rack1.drawMultipleLetters(LetterBag(), 7)
+        rackView.updateView(rack1.getRack())
         rackView.visibility = View.VISIBLE
         btnRefresh.setOnClickListener {
-            rack.exchangeLetters(letterbag, rack.getRack())
-            rackView.updateView(rack.getRack())
+            rack1.exchangeLetters(letterbag, rack1.getRack())
+            rackView.updateView(rack1.getRack())
         }
     }
 
