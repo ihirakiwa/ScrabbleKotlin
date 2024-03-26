73c7bdbb1a62e9c9af9d2aae7c97a7c4c00ec7b5
Matthieu <matthieu.espadamora@gmail.com>
2024-03-26 21:12:10 +0100

    meilleure version

---
+++ Scrabble/app/src/main/java/ControlsSection.kt
@@ -0,0 +1,38 @@
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ControlsSection(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(BUTTON_SPACING),
+            modifier = modifier.fillMaxWidth()
+        ) {
+            OutlinedButton(
+                onClick = {/*TODO*/},
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Resign")
+            }
+            OutlinedButton(
+                onClick = {/*TODO*/},
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Skip")
+            }
+            OutlinedButton(
+                onClick = {/*TODO*/},
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Swap")
+            }
+        }
+    }
+}
